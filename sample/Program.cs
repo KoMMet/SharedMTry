46039b89df6a21045d6c2b87d0ab24a037e3524b
守 小室 <hipukon2@gmail.com>
2018-10-28 18:59:43 +0900

    first commit

---
+++ sample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.MemoryMappedFiles;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open shared memory
+            MemoryMappedFile share_mem = MemoryMappedFile.CreateNew("shared_memory", 1024);
+            MemoryMappedViewAccessor accessor = share_mem.CreateViewAccessor();
+
+            // Write data to shared memory
+            string str = "Hello World";
+            char[] data = str.ToCharArray();
+            accessor.Write(0, data.Length);
+            accessor.WriteArray<char>(sizeof(int), data, 0, data.Length);
+
+            // Dispose accessor
+            accessor.Dispose();
+
+            // Sleep
+            while (true) System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
---
+++ sample2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryMappedFile share_mem = MemoryMappedFile.OpenExisting("shared_memory");
+            MemoryMappedViewAccessor accessor = share_mem.CreateViewAccessor();
+            CancellationTokenSource cts = new CancellationTokenSource();
+            bool stop = false;
+
+             var t1 = Task.Run(() => { WaitKey(cts); });
+            // Write data to shared memory
+            var t2 = Task.Run(() => { RunReading(share_mem,accessor, cts); });
+            Task.WaitAll(t1, t2);
+
+
+        }
+
+        private static void RunReading(MemoryMappedFile share_mem, MemoryMappedViewAccessor accessor, CancellationTokenSource cts)
+        {
+            while (true)
+            {
+                int size = accessor.ReadInt32(0);
+                if(size == 0) continue;
+                char[] data = new char[size];
+                accessor.ReadArray<char>(sizeof(int), data, 0, data.Length);
+                string str = new string(data);
+
+                Console.WriteLine("Data = " + str);
+                Thread.Sleep(1000);
+                if (cts.IsCancellationRequested)
+                {
+                    // Dispose resource
+                    accessor.Dispose();
+                    share_mem.Dispose();
+                    break;
+                }
+            }
+        }
+
+        private static void WaitKey(CancellationTokenSource ctsTs)
+        {
+            while(true)
+                if (Console.ReadKey().KeyChar == 'q')
+                {
+                    ctsTs.Cancel();
+                    break;
+                }
+        }
+    }
+}
